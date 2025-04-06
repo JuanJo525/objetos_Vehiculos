94afb97fea47467428cdf52519c8fd6202fafc37
juanmu525 <juanmu525@gmail.com>
2025-04-06 16:39:52 -0500

    a

---
+++ objetos_Vehiculos/ClsVehiculo.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace objetos_Vehiculos
+{
+    public class Cls_vehiculo
+    {
+
+        public string Matricula { get; set; }
+        public string Modelo { get; set; }
+        public int PotenciaCV { get; set; }
+
+        public Cls_vehiculo(string matricula, string modelo, int potencia)
+        {
+            Matricula = matricula;
+            Modelo = modelo;
+            PotenciaCV = potencia;
+        }
+
+        public string GetMatricula()
+        {
+            return Matricula;
+        }
+
+        public string GetModelo()
+        {
+            return Modelo;
+        }
+
+        public int GetPotencia()
+        {
+            return PotenciaCV;
+        }
+    }
+
+    public class ClsTaxi : Cls_vehiculo
+    {
+        public string numLicencia { get; set; }
+
+        public string getLicencia()
+        {
+            return numLicencia;
+        }
+
+        public ClsTaxi(string matricula, string modelo, int potencia) : base(matricula, modelo, potencia)
+        {
+            //
+        }
+
+        public void setLicencia(string licencia)
+        {
+            numLicencia = licencia;
+        }
+    }
+
+
+    public class ClsAutobus : Cls_vehiculo
+    {
+        public int numPlazas { get; set; }
+
+        public ClsAutobus(string matricula, string modelo, int potencia) : base(matricula, modelo, potencia)
+        {
+
+        }
+
+        public int getPlazas()
+        {
+            return numPlazas;
+        }
+
+        public void setPlazas(int plazas)
+        {
+            numPlazas = plazas;
+
+        }
+    }
+}
+
+
---
+++ objetos_Vehiculos/Program.cs
@@ -0,0 +1,86 @@
+﻿    using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace objetos_Vehiculos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int tipoVehiculo = 0;
+
+            while (tipoVehiculo != 3)
+            {
+                Console.Clear();
+                Console.WriteLine("Que tipo de vehiculo desea instanciar.");
+                Console.WriteLine("1: Taxi");
+                Console.WriteLine("2: Autobus");
+                Console.WriteLine("3: salir");
+                tipoVehiculo = int.Parse(Console.ReadLine());
+
+                Console.Clear();
+                switch (tipoVehiculo)
+                {
+                    case 1:
+                        Console.WriteLine("Instanciando un taxi");
+                        Console.WriteLine("ingrese matricula");
+                        string matricula = Console.ReadLine();
+                        Console.WriteLine("ingrese modelo");
+                        string modelo = Console.ReadLine();
+                        Console.WriteLine("ingrese potencia");
+                        int potencia = int.Parse(Console.ReadLine());
+
+                        ClsTaxi taxi = new ClsTaxi(matricula, modelo, potencia);
+
+                        Console.WriteLine("ingrese licencia");
+                        string licencia = Console.ReadLine();
+
+                        taxi.setLicencia(licencia);
+
+                        Console.Clear();
+                        Console.WriteLine("Datos del taxi;");
+                        Console.WriteLine("matricula: " + taxi.GetMatricula());
+                        Console.WriteLine("modelo: " + taxi.GetModelo());
+                        Console.WriteLine("potencia: " + taxi.GetPotencia());
+                        Console.WriteLine("licencia: " + taxi.getLicencia());
+                        break;
+
+                    case 2:
+                        Console.WriteLine("instanciando un autobus");
+                        Console.WriteLine("ingrese matricula");
+                        matricula = Console.ReadLine();
+                        Console.WriteLine("ingrese modelo");
+                        modelo = Console.ReadLine();
+                        Console.WriteLine("ingrese potencia");
+                        potencia = int.Parse(Console.ReadLine());
+
+                        ClsAutobus bus = new ClsAutobus(matricula, modelo, potencia);
+
+                        Console.WriteLine("ingrese cantidad de plazas");
+                        int pasajeros = int.Parse(Console.ReadLine());
+
+                        bus.setPlazas(pasajeros);
+
+                        Console.Clear();
+                        Console.WriteLine("datos del bus:");
+                        Console.WriteLine("matricula: " + bus.GetMatricula());
+                        Console.WriteLine("modelo: " + bus.GetModelo());
+                        Console.WriteLine("potencia: " + bus.GetPotencia());
+                        Console.WriteLine("plazas: " + bus.getPlazas());
+                        break;
+
+                    case 3:
+                        Console.WriteLine("saliendo ...");
+                    break;
+
+                    default: Console.WriteLine("Incorrecto"); break;
+                }
+                Console.WriteLine("Preciones enter para continuar");
+                Console.ReadKey();
+            }
+        }
+    }
+}
